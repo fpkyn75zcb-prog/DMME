17dbd0ba9655ce686285a947eb7e51fd965c9d21
Street <fpkyn75zcb@privaterelay.appleid.com>
2026-01-23 08:26:21 -0600

    Allow room routes

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,9 +1,16 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
 
+  // Let homepage load
   if (url.pathname === "/") {
     return context.next();
   }
 
+  // Let room pages load
+  if (url.pathname.startsWith("/room/")) {
+    return context.next();
+  }
+
+  // Everything else goes to Worker
   return context.env.WORKER.fetch(context.request);
 }
