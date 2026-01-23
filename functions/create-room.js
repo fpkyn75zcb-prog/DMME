3b1fd3a58b57a0cb9009b4c232ff7228264f1e45
Street <fpkyn75zcb@privaterelay.appleid.com>
2026-01-23 07:54:57 -0600

    Add create-room function to generate room IDs

---
+++ functions/create-room.js
@@ -0,0 +1,8 @@
+export async function onRequestPost() {
+  const roomId = crypto.randomUUID();
+
+  return new Response(
+    JSON.stringify({ roomId }),
+    { headers: { "Content-Type": "application/json" } }
+  );
+}
