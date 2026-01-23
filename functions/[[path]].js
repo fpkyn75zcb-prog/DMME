93490e6466f3b50a63be18c837ccab11aecf6722
Street <fpkyn75zcb@privaterelay.appleid.com>
2026-01-23 07:09:24 -0600

    Handle homepage requests in onRequest function

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,3 +1,11 @@
 export async function onRequest(context) {
+  const url = new URL(context.request.url);
+
+  // If someone is on the homepage, let Pages handle it
+  if (url.pathname === "/") {
+    return context.next();
+  }
+
+  // Everything else goes to the Worker
   return context.env.WORKER.fetch(context.request);
 }
