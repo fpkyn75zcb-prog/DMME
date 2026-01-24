037f10b28da20d754d3fd1b0a4ccb36e5aa33c6b
Street <fpkyn75zcb@privaterelay.appleid.com>
2026-01-24 00:11:25 -0600

    Update [roomId].js

--- functions/room/[roomId].js
+++ functions/room/[roomId].js
@@ -59,7 +59,7 @@ export async function onRequest() {
   flex: 1;
   height: 100%;
   background: transparent;
-  color: #b0b0b0;
+  color: #000000;
   border: none;
   outline: none;
   font-size: 16px;
